548b7e879df611b3e16bb332d1332b3ed2d2fc0d
nadja <nadja.merrild.k@hotmail.com>
2025-04-02 11:52:06 +0200

    init

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @var db $db
+ */
+
+require "settings/init.php";
+?>
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+    
+    <title>Sigende titel</title>
+    
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+    
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="images/logo.png" alt="logo"></a>
+
+    </div>
+</nav>
+
+<!--Hero-->
+
+<!--indhold-->
+
+<!--footer med kontakt information-->
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
